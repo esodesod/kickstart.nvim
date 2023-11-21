4a0f3263fdc41df86e9c8cc8400e51886432f8ab
Espen Ødegaard <esodesod@users.noreply.github.com>
2023-11-21 11:20:17 +0100

    add mappings for telescope and delete_buffer

--- init.lua
+++ init.lua
@@ -318,7 +318,10 @@ require('telescope').setup {
     mappings = {
       i = {
         ['<C-u>'] = false,
-        ['<C-d>'] = false,
+        ['<C-d>'] = require('telescope.actions').delete_buffer
+      },
+      n = {
+        ['<C-d>'] = require('telescope.actions').delete_buffer
       },
     },
   },
