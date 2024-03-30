7c357ffdcf87b3480e702eab47be57c401fc3ef0
Espen Ødegaard <esodesod@users.noreply.github.com>
2024-03-30 11:50:21 +0100

    Create bufferline.lua

---
+++ lua/custom/plugins/bufferline.lua
@@ -0,0 +1,15 @@
+return {
+  'akinsho/bufferline.nvim',
+  version = '*',
+  dependencies = 'nvim-tree/nvim-web-devicons',
+  config = function()
+    local bufferline = require 'bufferline'
+    bufferline.setup {
+      options = {
+        mode = 'buffers', -- use "buffers" (default) or "tabs" for tabpages only instead.
+      },
+    }
+    vim.keymap.set('n', '<Tab>h', ':BufferLineCyclePrev<CR>', { desc = 'bufferline cycle previous' })
+    vim.keymap.set('n', '<Tab>l', ':BufferLineCycleNext<CR>', { desc = 'bufferline cycle next' })
+  end,
+}
