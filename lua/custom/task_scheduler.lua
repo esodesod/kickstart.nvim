9314a1fb3fcb3d68fb7d9a5c05df8f6775679c66
Espen Ødegaard <esodesod@users.noreply.github.com>
2024-04-23 21:55:06 +0200

    task_scheduler is now local

--- lua/custom/task_scheduler.lua
+++ lua/custom/task_scheduler.lua
@@ -1,5 +1,9 @@
+local M = {}
+
 -- Function to toggle scheduled date of tasks to tomorrow
-function toggle_scheduled_date()
+function M.toggle_scheduled_date()
+  -- Get current line number
+  local current_line_number = vim.api.nvim_win_get_cursor(0)[1]
   -- Get current line
   local line = vim.fn.getline '.'
 
@@ -8,22 +12,36 @@ function toggle_scheduled_date()
   local scheduled_date = string.match(line, scheduled_date_pattern)
 
   if scheduled_date then
-    -- Convert scheduled date to a Lua date object
+    -- Extract year, month, and day
     local year, month, day = scheduled_date:match '(%d%d%d%d)%-(%d%d)%-(%d%d)'
-    local current_date = os.time { year = tonumber(year), month = tonumber(month), day = tonumber(day) }
 
-    -- Calculate tomorrow's date
-    local tomorrow_date = os.date('%Y-%m-%d', current_date + 86400)
+    -- Check if the extracted values are valid
+    if year and month and day then
+      -- Convert scheduled date to a Lua date object
+      local current_date = os.time { year = tonumber(year) or 0, month = tonumber(month) or 1, day = tonumber(day) or 1 }
 
-    -- Replace scheduled date with tomorrow's date
-    local new_line = line:gsub('(%d%d%d%d%-%d%d%-%d%d)$', tomorrow_date)
+      -- Check if current_date is valid
+      if current_date then
+        -- Calculate tomorrow's date
+        local tomorrow_date = os.date('%Y-%m-%d', current_date + 86400)
 
-    -- Update the line in the buffer
-    vim.fn.setline('.', new_line)
+        -- Replace scheduled date with tomorrow's date
+        local new_line = line:gsub('(%d%d%d%d%-%d%d%-%d%d)$', tomorrow_date)
+
+        -- Update the line in the buffer
+        vim.fn.setline(current_line_number, new_line)
+      else
+        print 'Invalid date components extracted from scheduled date.'
+      end
+    else
+      print 'Invalid format for scheduled date.'
+    end
   else
     print 'Scheduled or due date not found in line.'
   end
 end
 
 -- Mapping to trigger the toggle function
-vim.api.nvim_set_keymap('n', '<leader>tom', ':lua toggle_scheduled_date()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>tom', ':lua require"custom.task_scheduler".toggle_scheduled_date()<CR>', { noremap = true, silent = true })
+
+return M
