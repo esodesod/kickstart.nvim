31a475d6d0e8a2805aeff69cbefc71891aaadf22
Espen Ødegaard <esodesod@users.noreply.github.com>
2024-05-03 10:24:42 +0200

    personalized keymaps for buffers and tabs a bit more

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -1,17 +1,24 @@
--- jump around go to definition
-vim.keymap.set('n', 'gd', '<C-]>', { desc = 'Jump to the [d]efinition of the keyword' })
+-- buffers
+vim.keymap.set('n', '<C-n>', ':bnext<enter>', { desc = 'next buffer' })
+vim.keymap.set('n', '<C-p>', ':bprev<enter>', { desc = 'previous buffer' })
+vim.keymap.set('n', 'bd', ':bdelete<enter>', { desc = '[b]uffer [d]elete' })
+vim.keymap.set('n', 'bD', ':bdelete!<enter>', { desc = '[b]uffer [D]elete!' })
+
+-- tabs
+vim.keymap.set('n', 'H', ':tabprev<enter>', { desc = 'previous tab' })
+vim.keymap.set('n', 'L', ':tabnext<enter>', { desc = 'next tab' })
+vim.keymap.set('n', 'tn', ':tabnew <cr>', { desc = '[t]ab [n]ew' })
+vim.keymap.set('n', 'tc', ':tabclose <cr>', { desc = '[t]ab [c]lose' })
+
+-- window
+vim.keymap.set('n', '-', ':new<cr>', { desc = '[n]ew window' })
+vim.keymap.set('n', '|', ':vnew<cr>', { desc = '[v]ertical [n]ew split window' })
+
+-- jump around
+vim.keymap.set('n', 'gd', '<C-]>', { desc = 'Jump to the [d]efinition of the keyword under the cursor' })
 
 -- terminal
 vim.keymap.set('t', '<C-x>', '<C-\\><C-n>', { desc = 'Escape terminal mode' })
 vim.keymap.set('n', 'tt', ':tabnew +term<enter>', { desc = 'tabnew +term' })
 vim.keymap.set('n', 'th', ':split +term<enter>', { desc = 'split +term' })
 vim.keymap.set('n', 'tv', ':vsplit +term<enter>', { desc = 'vsplit +term' })
-
--- navigate and edit buffers
-vim.keymap.set('n', 'tk', ':bnext<enter>', { desc = 'Buffer Next' })
-vim.keymap.set('n', 'tj', ':bprev<enter>', { desc = 'Buffer Previous' })
-vim.keymap.set('n', 'td', ':bdelete<enter>', { desc = 'Buffer Delete' })
-
--- window
-vim.keymap.set('n', '-', ':new<cr>', { desc = '[n]ew window' })
-vim.keymap.set('n', '|', ':vnew<cr>', { desc = '[v]ertical [n]ew split window' })
