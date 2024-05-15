d30548324dcd014972f7a49309aa209f77aa8c9b
Espen Ødegaard <esodesod@users.noreply.github.com>
2024-05-15 13:39:50 +0200

    disabled bufferline, for now

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -1,15 +1,15 @@
 return {
-  'akinsho/bufferline.nvim',
-  version = '*',
-  dependencies = 'nvim-tree/nvim-web-devicons',
-  config = function()
-    local bufferline = require 'bufferline'
-    bufferline.setup {
-      options = {
-        mode = 'tabs', -- use "buffers" (default) or "tabs" for tabpages only instead.
-      },
-    }
-    vim.keymap.set('n', '<Tab>h', ':BufferLineCyclePrev<CR>', { desc = 'bufferline cycle previous' })
-    vim.keymap.set('n', '<Tab>l', ':BufferLineCycleNext<CR>', { desc = 'bufferline cycle next' })
-  end,
+  -- 'akinsho/bufferline.nvim',
+  -- version = '*',
+  -- dependencies = 'nvim-tree/nvim-web-devicons',
+  -- config = function()
+  --   local bufferline = require 'bufferline'
+  --   bufferline.setup {
+  --     options = {
+  --       mode = 'tabs', -- use "buffers" (default) or "tabs" for tabpages only instead.
+  --     },
+  --   }
+  --   vim.keymap.set('n', '<Tab>h', ':BufferLineCyclePrev<CR>', { desc = 'bufferline cycle previous' })
+  --   vim.keymap.set('n', '<Tab>l', ':BufferLineCycleNext<CR>', { desc = 'bufferline cycle next' })
+  -- end,
 }
