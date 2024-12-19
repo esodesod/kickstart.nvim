ef9ca8ffdd215d0fc1fbd4e3d0c90599193c37a4
Espen Ødegaard <esodesod@users.noreply.github.com>
2024-12-19 21:15:20 +0100

    treesitter-context: max_lines to 2

--- lua/custom/plugins/treesitter-context.lua
+++ lua/custom/plugins/treesitter-context.lua
@@ -3,6 +3,9 @@ return {
     'nvim-treesitter/nvim-treesitter-context',
     opts = {
       enabled = true,
+      max_lines = 2,
+      -- multiline_threshold = 20,
+      -- min_window_height = 2,
     },
   },
 }
