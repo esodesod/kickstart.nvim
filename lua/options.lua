5979cea9407c643022fe1fa282092c28e545d29b
Espen Ødegaard <esod@esod.no>
2025-11-05 09:07:57 +0100

    expandtab, tabstop and shiftwidth

--- lua/options.lua
+++ lua/options.lua
@@ -76,4 +76,9 @@ end
 -- Auto change workdir when opening a file, buffer, etc.
 vim.opt.autochdir = false
 
+vim.opt.expandtab = true -- convert tabs to spaces
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+
 -- vim: ts=2 sts=2 sw=2 et
